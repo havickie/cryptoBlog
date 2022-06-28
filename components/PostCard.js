4d5b50f835d75878e4dd95d57e95f191e7459969
victor <victorfrancis612@gmail.com>
2022-06-28 15:52:34 +0100

    commit

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import styles from "../styles/BlogCard.module.css";
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import moment from "moment";
 
 const PostCard = ({ post }) => {
   return (
@@ -10,17 +11,32 @@ const PostCard = ({ post }) => {
     //     <img layout="cover" src={post.featuredImage.url} alt={post.title} />
     //   </div>
     // </div>
-    <div className="container">
- <Card style={{ width: '22rem' }}>
-  <Card.Img variant="top" src={post.featuredImage.url} />
-  <Card.Body>
-        <Card.Title>{ post.title }</Card.Title>
-    <Card.Text>
-          { post.excerpt }
-    </Card.Text>
-    <Button variant="primary">Read More</Button>
-  </Card.Body>
-</Card>
+   
+    <div className={`container text-decoration-none ${styles.card} `}>
+        
+      <div className={styles.imgContainer}>
+      <Link className="a" href={`/post/${post.slug}`}>
+<img src={post.featuredImage.url} alt={post.title} />
+        </Link>
+</div>
+      <h1 className={styles.h1}>
+        <Link className="text-decoration-none" href={`/post/${post.slug}`}>
+{post.title}
+        </Link>
+      </h1>
+      <div className={styles.excerpt}>
+        <p>
+          <Link href={`/post/${post.slug}`}>
+            {post.excerpt}
+          </Link>
+</p>
+      </div>
+      <div >
+         <span className={styles.date}>
+        {moment(post.createdAt).format('MMM DD, YYYY')}
+      </span>
+ </div>
+     
     </div>
    
   );
--- services/index.js
+++ services/index.js
@@ -4,28 +4,71 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
   const query = gql`
-  query MyQuery {
-    postsConnection {
-      edges {
-        node {
-          createdAt
-          slug
-          title
-          excerpt
-          featuredImage {
-            url
-          }
-          categories {
-            name
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            createdAt
             slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
           }
         }
       }
     }
-  }
-  
   `;
   const result = await request(graphqlAPI, query);
 
   return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+query GETPostDetails(){
+posts(orderBy: createdAt_ASC
+  last: 3
+  ) {
+    title
+    featuredImage{
+      url
+    }
+    slug
+  }
+}
+
+`;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
 };
